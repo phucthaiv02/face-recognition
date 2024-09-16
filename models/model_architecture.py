d644ba2ef3711d167c81d9c5ab2046b94141a54f
De Pie <phucthaiv02@gmail.com>
2024-09-16 22:14:08 +0700

    Define new model architecture to determine 2 images are the same person or not

--- models/model_architecture.py
+++ models/model_architecture.py
@@ -1,21 +1,43 @@
 import torch
 from torch import nn
+import torch.nn.functional as F
 from facenet_pytorch import InceptionResnetV1
 
 
-class classifyModel(nn.Module):
-    def __init__(self, num_classes):
-        super(classifyModel, self).__init__()
+class FaceNet(nn.Module):
+    def __init__(self):
+        super(FaceNet, self).__init__()
         self.resnet = InceptionResnetV1(pretrained='vggface2').eval()
-        self.fc = nn.Sequential(
-            nn.Linear(512, num_classes),
-            nn.Softmax(dim=1)
-        )
 
     def forward(self, x):
         with torch.no_grad():
             embeddings = self.resnet(x)
 
-        output = self.fc(embeddings)
+        return embeddings
 
+
+class SiameseNetwork(nn.Module):
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        self.resnet = InceptionResnetV1(pretrained='vggface2')
+        self.fc = nn.Sequential(
+            nn.Linear(2 * 512, 512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, 1),
+            nn.Sigmoid()
+        )
+
+    def forward(self, input1, input2):
+        with torch.no_grad():
+            embedding1 = self.resnet(input1)
+            embedding2 = self.resnet(input2)
+
+        combined_embeddings = torch.cat((embedding1, embedding2), dim=1)
+
+        output = self.fc(combined_embeddings)
+        output = output.squeeze(1)
         return output
--- scripts/train.py
+++ scripts/train.py
@@ -1,16 +1,22 @@
 import torch
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-def train(model, train_loader, val_loader, epochs, criterion, optimizer):
+
+def train(model, train_loader, val_loader, epochs, criterion, optimizer, scheduler=None):
     history = {'train_loss': [], 'val_loss': []}
+
     for epoch in range(1, epochs + 1):
         print(f'Epoch {epoch}/{epochs}: ', end='')
         model.train()
         train_loss = 0.0
-        for i, (images, labels) in enumerate(train_loader):
+        for idx, batch in enumerate(train_loader):
+            images1 = batch[0].to(device)
+            images2 = batch[1].to(device)
+            labels = batch[2].to(device)
             optimizer.zero_grad()
 
-            output = model(images)
+            output = model(images1, images2)
             loss = criterion(output, labels)
 
             loss.backward()
@@ -22,6 +28,10 @@ def train(model, train_loader, val_loader, epochs, criterion, optimizer):
         val_loss = val(model, val_loader, criterion)
         print(f'train_loss: {train_loss}, val_loss: {val_loss}')
 
+        if scheduler:
+            scheduler.step(val_loss)
+            print(f"\tLearning rate: {scheduler.get_last_lr()}")
+
         torch.save(model, f'./output/checkpoints/ckp{epoch}.pth',)
     torch.save(model, f'output/model/model.pth')
 
@@ -32,8 +42,11 @@ def val(model, val_loader, criterion):
     model.eval()
 
     val_loss = 0.0
-    for i, (images, labels) in enumerate(val_loader):
-        output = model(images)
+    for idx, batch in enumerate(val_loader):
+        images1 = batch[0].to(device)
+        images2 = batch[1].to(device)
+        labels = batch[2].to(device)
+        output = model(images1, images2)
         loss = criterion(output, labels)
         val_loss += loss.item()
 
